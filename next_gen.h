dd0a83d57522b406e867002796d029c7314f3fb1
charlyfigue <cfigueroa@itba.edu.ar>
2019-05-08 12:12:32 -0300

    funcion de actualizacion

---
+++ next_gen.h
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   next_gen.h
+ * Author: enzo
+ *
+ * Created on 8 de mayo de 2019, 11:33
+ */
+
+#ifndef NEXT_GEN_H
+#define NEXT_GEN_H
+
+//usando la informacion guardada en cada posicion, actualiza 
+//el estado de las celulas.
+
+void next_gen (int mat[N][M]);
+
+#endif
+
