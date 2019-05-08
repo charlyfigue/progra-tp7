698122fc74d093eee3036490f5709bf840e2e529
charlyfigue <cfigueroa@itba.edu.ar>
2019-05-08 12:18:24 -0300

    Impresion

---
+++ print_mat.h
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   print_mat.h
+ * Author: enzo
+ *
+ * Created on 8 de mayo de 2019, 11:29
+ */
+
+#ifndef PRINT_MAT_H
+#define PRINT_MAT_H
+
+//imprime la matriz de la nueva generacion, usa @ para indicar vivos 
+//y espacios para indicar muertos
+
+void print_mat (int mat[N][M]);
+
+#endif
+
+
